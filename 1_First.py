328ebf79f6bf3561a939658cc7de6b54f1372350
mdfazal <fazalmd964@gmail.com>
2021-01-19 02:16:10 +0530

    Learning Python

---
+++ 1_First.py
@@ -0,0 +1,7 @@
+character_name = 'Tom'
+character_age = '25'
+print ('There was once a man name ' + character_name )
+print ('He was ' +character_age+ ' yeras old ')
+character_name = 'Mike'
+print ('He really liked the name ' + character_name)
+print ('but didnt like being '+character_age)

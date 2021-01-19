8d99a8850b667ea23155f63161edf83f7f6f7874
mdfazal <fazalmd964@gmail.com>
2021-01-19 13:25:43 +0530

    Learning Python

---
+++ 10_functions.py
@@ -0,0 +1,13 @@
+def sayhi(name, age):
+    print("Hello "+ name+ " you are "+ age)
+
+print("top")
+sayhi("Mike", "35")
+sayhi("Steve", "70")
+print("bottom")
+
+def say_hi(name, age):
+    print("Hello "+ name+ " you are "+ str(age))
+
+say_hi("Mike", 35)
+say_hi("Steve", 70)

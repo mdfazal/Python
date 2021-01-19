af672b75560facd815777f7fbfb2b856b4b6ba6a
mdfazal <fazalmd964@gmail.com>
2021-01-19 13:33:41 +0530

    Learning Python

---
+++ 17_For_Loops.py
@@ -0,0 +1,23 @@
+for letters in "Giraffe Academy":
+    print(letters)
+
+
+friends = ["Jim", "Karen", "Kevin"]
+for friend in friends:
+    print(friend)
+
+for i in range (10):
+    print(i)
+
+for j in range (3, 10):
+    print(j)
+
+friends = ["Jim", "Karen", "Kevin"]
+for index in range(len(friends)):
+    print(friends[index])
+
+for k in range(5):
+    if k == 0:
+        print ("First interation")
+    else:
+        print("Not first")

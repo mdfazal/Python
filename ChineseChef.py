f471efe1f38438a2d4b9e9d869c20fdfd3ceda87
mdfazal <fazalmd964@gmail.com>
2021-01-19 13:46:28 +0530

    Learning Python

---
+++ ChineseChef.py
@@ -0,0 +1,9 @@
+from Chef import Chef 
+
+class ChineseChef(Chef):
+
+    def makes_special_dish(self):
+        print("The chef make orange chicken")
+
+    def make_fried_rice(self):
+        print("The chef makes Fried Rice")

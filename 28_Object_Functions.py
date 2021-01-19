1f470790ac07c8bda57855e993953c86ce16b93b
mdfazal <fazalmd964@gmail.com>
2021-01-19 13:45:20 +0530

    Learning Python

---
+++ 28_Object_Functions.py
@@ -0,0 +1,8 @@
+from student import student
+
+student1 = student("Oscar", "Accounting", 3.1, True)
+student2 = student("Pam", "Art", 3.8, False)
+
+print(student2.on_honor_roll())
+print(student1.on_honor_roll())
+

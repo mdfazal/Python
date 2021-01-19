1f884515c6d99b655cbaf9e48f36a94440761a4e
mdfazal <fazalmd964@gmail.com>
2021-01-19 13:34:27 +0530

    Learning Python

---
+++ 20_Building_A_Translator.py
@@ -0,0 +1,15 @@
+#every vowels becomes g for eg - Cat -> Cgt
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
+        

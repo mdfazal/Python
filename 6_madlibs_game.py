e0306733c0782a1ec8766c27946c3b91fa573c13
mdfazal <fazalmd964@gmail.com>
2021-01-19 13:22:07 +0530

    Learning Python

---
+++ 6_madlibs_game.py
@@ -0,0 +1,7 @@
+color = input('Enter a color: ')
+plural_noun = input('Enter a plural noun: ')
+celeb = input('Enter a celeb: ')
+
+print("Roses are "+color)
+print(plural_noun+ " are blue")
+print("I Love "+celeb)

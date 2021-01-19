66372a78021dfffef0db074824e1449a00e8b84c
mdfazal <fazalmd964@gmail.com>
2021-01-19 13:46:12 +0530

    Learning Python

---
+++ Chef.py
@@ -0,0 +1,11 @@
+class Chef:
+
+    def make_chicken(self):
+        print("The chef makes chicken")
+
+    def make_salad(self):
+        print("The chef makes salad")
+
+    def makes_special_dish(self):
+        print("the chef makes bbq ribs")
+

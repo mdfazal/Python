d7d75d2e406aa879ea00c0621d4ac3d802110451
mdfazal <fazalmd964@gmail.com>
2021-01-25 20:39:45 +0530

    learn python

--- 4_Getting_Input.py
+++ 4_Getting_Input.py
@@ -1,3 +1,9 @@
+hrs = input("Enter Hours:")
+hour = float(hrs) 
+rate = 2.75
+pay = rate*hour
+print("Pay: "+ str(pay))
+
 name = input("Enter your name: ")
 age = input("Enter your age")
 print("Hello "+name+ " you are "+age)
